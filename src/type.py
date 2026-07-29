6925e9ca89df75b5f3ee248d7b9ab6ae8a7af029
Taha-Azadi <tt7097447@gmail.com>
2026-07-29 22:03:01 +0330

    Update type.py

--- src/type.py
+++ src/type.py
@@ -20,15 +20,11 @@ class Type:
 
             if self.text != None:
                 print(f"user: {self.text}")
-                if "open" in self.text.lower():
-                    open_any_type(self)
                 if "bye" in self.text.lower():
                     generate_and_bye_type(self)
                     break
                 if "the time" in self.text.lower() or "time is" in self.text.lower():
                     time_type(self)
-                elif "hello" in self.text.lower() or "hi " in self.text.lower():
-                    generate_and_hello_type(self)
                 elif "clear" in self.text.lower() or "cls " in self.text.lower():
                     clear()
                 elif any(
