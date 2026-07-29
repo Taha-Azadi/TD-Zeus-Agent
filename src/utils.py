a96c8a314f9f92ae7b3e530e03b08130c9e2bd17
Taha-Azadi <tt7097447@gmail.com>
2026-07-29 09:49:11 +0330

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -374,7 +374,7 @@ def _stream_chat_completion(payload, on_token, on_thinking_done=None):
     return full_text, full_thinking, tool_calls_buffer
 
 
-def _run_conversation_with_tools(messages, model="gnvidia/nemotron-3-ultra-550b-a55b:free",
+def _run_conversation_with_tools(messages, model="nvidia/nemotron-3-ultra-550b-a55b:free",
                                   on_token=None, on_thinking_done=None, max_iterations=5):
     iteration = 0
     final_content = ""
